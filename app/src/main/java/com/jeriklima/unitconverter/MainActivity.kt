57fd326c65a7ca468514c664aeea802a70a358de
Jeriklima <jonherik123@gmail.com>
2024-01-12 15:39:00 -0300

    Criação do dropdownmenu e alinhamento

--- app/src/main/java/com/jeriklima/unitconverter/MainActivity.kt
+++ app/src/main/java/com/jeriklima/unitconverter/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.jeriklima.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jeriklima.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,18 +51,50 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun UnitConverter(){
-    Column {
+fun UnitConverter() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
         //all the UI Elements will be stacked below each other
-    Text("Unit Converter")
+        Text("Unit Converter")
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(value = "", onValueChange = {//dasjdas
-             })
+        })
+        Spacer(modifier = Modifier.height(16.dp))
 
         Row {
-    Button(onClick = { /*TODO*/ }) {
-        Text(text = "Click Me!")
-    }
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+                }
+                DropdownMenu(expanded = true, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Meters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Feet") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Milimeters") }, onClick = { /*TODO*/ })
+
+                }
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+                }
+                DropdownMenu(expanded = true, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Meters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Feet") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Milimeters") }, onClick = { /*TODO*/ })
+
+                }
+            }
         }
+        Spacer(modifier = Modifier.height(16.dp))
         Text("Result:")
     }
 }
@@ -55,6 +102,6 @@ fun UnitConverter(){
 
 @Preview(showBackground = true)
 @Composable
-fun UnitConverterPreview(){
+fun UnitConverterPreview() {
     UnitConverter()
 }
